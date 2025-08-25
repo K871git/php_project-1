dd17af21f44cebb54cd4ec9dd030e148bbcf673e
Spark871 <124434561+K871git@users.noreply.github.com>
2025-08-25 23:18:59 +0530

    Update ProfileController.php

--- upcomingProject/laravelPractices/customDash/app/Http/Controllers/ProfileController.php
+++ upcomingProject/laravelPractices/customDash/app/Http/Controllers/ProfileController.php
@@ -1,4 +1,4 @@
-<?php
+7<?php
 
 namespace App\Http\Controllers;
 
@@ -45,3 +45,5 @@ class ProfileController extends Controller
         return back()->with('success', 'Profile updated successfully.');
     }
 }
+
+# Php scripts 
